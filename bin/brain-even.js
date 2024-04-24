b3b5c5dd4404804607010580e3d7f1c8b3e07ea0
Valeriy Bulakaev <friestft@gmail.com>
2024-04-24 13:59:57 +0300

    brain-even little fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,22 +1,18 @@
 #!/usr/bin/env node
-// import readlineSync from 'readline-sync';
-// import cli from '../src/cli.js';
 import gameBody from '../src/gameBody.js';
-
-// const userName = cli();
 
 const decription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const task = () => {
-  const q = Math.round(Math.random() * 100);
-  // const a = q % 2; // Вернет 1 если нечет и вернет 0 если чет
-  let a = '';
-  if (q % 2 === 0) {
-    a = 'yes';
-  } else {
-    a = 'no';
-  }
-  return [q, a];
+    const q = Math.round(Math.random() * 100);
+    // const a = q % 2; // Вернет 1 если нечет и вернет 0 если чет
+    let a = '';
+    if (q % 2 === 0) {
+        a = 'yes';
+    } else {
+        a = 'no';
+    }
+    return [q, a];
 };
 
 gameBody(decription, task);
